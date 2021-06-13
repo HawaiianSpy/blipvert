e215c40a0642ce29157b197a89c320a11f3454ca
donj <hawaiianspy@protonmail.com>
2021-06-12 20:25:33 -0700

    Added front-end unit tests for library.

--- UnitTests/FindVideoTransformUnitTests.cpp
+++ UnitTests/FindVideoTransformUnitTests.cpp
@@ -12,7 +12,7 @@ using namespace blipvert;
 
 namespace BlipvertUnitTests
 {
-	TEST_MODULE_INITIALIZE(FindVideoTransform_ModuleInit)
+	TEST_MODULE_INITIALIZE(BlipvertUnitTests_ModuleInit)
 	{
 		InitializeLibrary();
 	}
@@ -24,18 +24,64 @@ namespace BlipvertUnitTests
 		TEST_METHOD(FindValidTransform_UnitTest)
 		{
 			t_transformfunc func = FindVideoTransform(MVFMT_UYVY, MVFMT_RGB32);
-			Assert::IsNotNull(reinterpret_cast<void*>(func), L"FindValidTransform_UnitTest returned a null function pointer.");
+			Assert::IsNotNull(reinterpret_cast<void*>(func), L"FindVideoTransform returned a null function pointer.");
 			Assert::AreEqual(reinterpret_cast<void*>(UYVY_to_RGB32), reinterpret_cast<void*>(func), L"FindValidTransform_UnitTest returned the wrong function pointer.");
 
 			func = FindVideoTransform(MVFMT_cyuv, MVFMT_Y411);
-			Assert::IsNotNull(reinterpret_cast<void*>(func), L"FindValidTransform_UnitTest returned a null function pointer.");
+			Assert::IsNotNull(reinterpret_cast<void*>(func), L"FindVideoTransform returned a null function pointer.");
 			Assert::AreEqual(reinterpret_cast<void*>(UYVY_to_IYU1), reinterpret_cast<void*>(func), L"FindValidTransform_UnitTest returned the wrong function pointer.");
 		}
 
 		TEST_METHOD(FindInvalidTransform_UnitTest)
 		{
 			t_transformfunc func = FindVideoTransform(MVFMT_UYVY, MVFMT_Plum);
-			Assert::IsNull(reinterpret_cast<void*>(func), L"FindInvalidTransform_UnitTest returned a non-null function pointer.");
+			Assert::IsNull(reinterpret_cast<void*>(func), L"FindVideoTransform returned a non-null function pointer.");
+		}
+	};
+
+	TEST_CLASS(GetVideoFormatInfo_UnitTests)
+	{
+	public:
+
+		TEST_METHOD(GetValidVideoFormatInfo_UnitTest)
+		{
+			Fourcc outfourcc;
+			Fourcc outxRefFourcc;
+			int16_t outeffctiveBitsPerPixel;
+
+			Assert::IsTrue(GetVideoFormatInfo(MVFMT_YUNV, outfourcc, outxRefFourcc, outeffctiveBitsPerPixel), L"GetVideoFormatInfo: Expected to find format info.");
+			Assert::AreEqual(FOURCC_YUNV, outfourcc, L"GetVideoFormatInfo: Wrong fourcc returned for media ID.");
+			Assert::AreEqual(FOURCC_YUY2, outxRefFourcc, L"GetVideoFormatInfo: Wrong cross-referenced fourcc returned for media ID.");
+			Assert::AreEqual(static_cast<int32_t>(outeffctiveBitsPerPixel), 16, L"GetVideoFormatInfo: Wrong bpp returned for media ID.");
+		}
+
+		TEST_METHOD(GetInvalidVideoFormatInfo_UnitTest)
+		{
+			Fourcc outfourcc;
+			Fourcc outxRefFourcc;
+			int16_t outeffctiveBitsPerPixel;
+
+			Assert::IsFalse(GetVideoFormatInfo(MVFMT_UNDEFINED, outfourcc, outxRefFourcc, outeffctiveBitsPerPixel), L"GetVideoFormatInfo: Expected to not find format info.");
+		}
+	};
+
+	TEST_CLASS(GetVideoFormatID_UnitTests)
+	{
+	public:
+
+		TEST_METHOD(GetValidVideoFormatID_UnitTest)
+		{
+			MediaFormatID outMediaID;
+
+			Assert::IsTrue(GetVideoFormatID(FOURCC_YUY2, outMediaID), L"GetVideoFormatID: Did not return a media ID.");
+			Assert::AreEqual(MVFMT_YUY2, outMediaID, L"GetVideoFormatID did not return the correct media ID.");
+		}
+
+		TEST_METHOD(GetInvalidVideoFormatID_UnitTest)
+		{
+			MediaFormatID outMediaID;
+
+			Assert::IsFalse(GetVideoFormatID(FOURCC_UNDEFINED, outMediaID), L"GetVideoFormatID: Returned a valid media ID.");
 		}
 	};
 }

809e8627e0d87247804437ddedc12439ebe4cda7
donj <hawaiianspy@protonmail.com>
2021-07-02 20:54:37 -0700

    Deleted unecessary perens.

--- blipvert/Utilities.cpp
+++ blipvert/Utilities.cpp
@@ -61,7 +61,7 @@ void blipvert::SlowYUVtoRGB(double Y, double U, double V, double* R, double* G, 
 void blipvert::SlowRGBtoYUV(double R, double G, double B, double* Y, double* U, double* V)
 {
     *Y = 0.257 * R + 0.504 * G + 0.098 *B + 16;
-    *U = -(0.148 * R) - 0.291 * G + 0.439 * B + 128;
+    *U = -0.148 * R - 0.291 * G + 0.439 * B + 128;
     *V = 0.439 * R - 0.368 * G - 0.071 * B + 128;
 }
 
